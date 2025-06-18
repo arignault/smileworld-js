f9e6087ea119db6084b266948b72995c1f7db8b6
Paul Rouet <paul.rouet@gmail.com>
2025-06-18 10:48:07 -0700

    feat: Mettre en place Vite.js pour le build

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'main_gsap.js'),
+      name: 'SmileWorld',
+      // Le nom du fichier de sortie
+      fileName: 'smileworld-bundle',
+    },
+    rollupOptions: {
+      // Pas besoin d'externaliser de dépendances ici pour l'instant
+      external: [],
+      output: {
+        // Exposer les variables globales pour les bibliothèques UMD
+        globals: {},
+      },
+    },
+    // Générer le bundle dans un dossier 'dist' à la racine
+    outDir: 'dist', 
+  },
+}); 
